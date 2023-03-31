944636d63785a286ca5d52096172d47f10c18885
kevinliu69 <99049405+kevinliu69@users.noreply.github.com>
2023-03-31 19:42:31 +0100

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAk_gg7lHoMJdVliaKbiUdJ35id.Dg-P9s",
+    authDomain: "social-e5855.firebaseapp.com",
+    databaseURL: "https://social-e5855-default-rtdb.firebaseio.com",
+    projectId: "social-e5855",
+    stroageBucket: "social-e5855.appspot.com",
+    messagingSenderId: "671543009162",
+    appId "1:67154300162:web:a881fdddf02bf05c08b273",
+};
+
